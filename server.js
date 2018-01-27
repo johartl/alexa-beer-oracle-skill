f2b1439e7ec5ba39a8f76dc3345032bbdf7fa68c
Jochen Hartl <jochen.hartl@tum.de>
2018-01-27 19:44:22 +0100

    First prototype of skill

---
+++ server.js
@@ -0,0 +1,21 @@
+const AlexaSkillServer = require('alexa-skill-server');
+
+const answers = {};
+answers['en'] = ['Yes!', 'Of course!', 'Definitely!', 'Absolutely!', 'Do it!',
+    'Sure thing!', 'By all means!', 'Most definitely!'];
+
+answers['de'] = ['Ja!', 'Unbedingt!', 'Auf jeden Fall', 'Natürlich!',
+    'Zweifelsfrei!', 'Worauf wartest du noch?', 'Ich sehe nichts, was dagegen spricht!'];
+
+class BeerOClockSkillServer extends AlexaSkillServer {
+
+    onStartIntentRequest(request) {
+        const language = request.locale.split('-')[0];
+        const localeAnswers = answers[language] || answers['en'];
+        const selectedAnswer = localeAnswers[Math.floor(Math.random()*localeAnswers.length)];
+
+        return this.respond().say(selectedAnswer).endSession();
+    }
+}
+
+(new BeerOClockSkillServer()).start();
