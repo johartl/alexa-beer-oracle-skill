1cf8ed53b9114c3a245a63354ffe8bfa4cda82e5
Jochen Hartl <jochen.hartl@tum.de>
2018-01-28 14:32:15 +0100

    Add intents for languages EN and DE

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ answers['en'] = ['Yes!', 'Of course!', 'Definitely!', 'Absolutely!', 'Do it!',
 answers['de'] = ['Ja!', 'Unbedingt!', 'Auf jeden Fall', 'Natürlich!',
     'Zweifelsfrei!', 'Worauf wartest du noch?', 'Ich sehe nichts, was dagegen spricht!'];
 
+
 class BeerOClockSkillServer extends AlexaSkillServer {
 
     onStartIntentRequest(request) {
-        const language = request.locale.split('-')[0];
+        const [language, dialect] = request.locale.split('-');
         const localeAnswers = answers[language] || answers['en'];
         const selectedAnswer = localeAnswers[Math.floor(Math.random()*localeAnswers.length)];
 
